4c260cec113d435226858ae14aa0772b5941abac
wratterman <will.ratterman@gmail.com>
2017-06-06 09:10:28 -0600

    stock now an initialized attribute of Panty, empty hash at first

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,9 @@
 class Pantry
 
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -9,4 +9,11 @@ class PantryTest < Minitest::Test
 
     assert_instance_of Pantry, a
   end
+
+  def test_it_has_stock
+    a = Pantry.new
+
+    assert_instance_of Hash, a.stock
+    assert_equal 0, a.stock.length
+  end
 end
